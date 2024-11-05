1795747bc0deb441742c5343487c2d1444849c40
breadbread1984 <breadbread1984@163.com>
2024-11-05 02:16:17 +0000

    updaste

--- test/sem_seg.py
+++ test/sem_seg.py
@@ -13,7 +13,7 @@ class TestSemanticSeg(unittest.TestCase):
   def test_function(self):
     tokenizer, llm = Qwen2(locally = True)
     semantic_seg = load_semantic_seg(tokenizer, llm, 'zh')
-    with open('sem_seg.txt', 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), 'sem_seg.txt'), 'r') as f:
       res = semantic_seg.invoke({'input': f.read()})
     print(res)
 
