c0d7d217cb4dd078b57855c34aee93098a35df06
breadbread1984 <breadbread1984@163.com>
2024-11-05 07:14:03 +0000

    update

--- tools/spg_extract/spg_extract.py
+++ tools/spg_extract/spg_extract.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
+from .prompts import spg_extract
 
+class SPGExtract(object):
+  def __init__(self, tokenizer, llm, schema):
+    template, parser = spg_extract(tokenizer, schema)
+    self.chain = template | llm | parser
+  def predict(self, query: str):
+    return self.chain.invoke({'input': query})
