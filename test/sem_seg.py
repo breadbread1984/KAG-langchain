2c2e6ef71b2f4c6f1c69d2b39dc8495109deef8a
breadbread1984 <breadbread1984@163.com>
2024-11-05 02:06:56 +0000

    update

--- test/sem_seg.py
+++ test/sem_seg.py
@@ -3,10 +3,10 @@
 import sys
 import os
 import unittest
-from models import Qwen2
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from models import Qwen2
 from tools import load_semantic_seg
 
 class TestSemanticSeg(unittest.TestCase):
