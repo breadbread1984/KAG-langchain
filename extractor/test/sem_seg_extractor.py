8b3521969df428e3e2bfbd874d57f6f59fffdcba
breadbread1984 <breadbread1984@163.com>
2024-11-07 02:54:47 +0000

    update

---
+++ extractor/test/sem_seg_extractor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from sem_seg_extractor import SemanticSegmentExtractor
+
+class TestSemanticSegExtractor(unittest.TestCase):
+  def test_function(self):
+    extractor = SemanticSegmentExtractor()
+    file_path = os.path.join(os.path.dirname(__file__), 'sem_seg.txt')
+    segments = extractor.extract(file_path)
+    print(segments)
+
+if __name__ == "__main__":
+  unittest.main()
