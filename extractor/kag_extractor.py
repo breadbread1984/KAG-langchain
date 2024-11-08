d312f0b1962892c55bff2ef1ff753347b05195fb
breadbread1984 <breadbread1984@163.com>
2024-11-08 07:58:25 +0000

    update

--- extractor/kag_extractor.py
+++ extractor/kag_extractor.py
@@ -32,11 +32,11 @@ class KAGExtractor(object):
   def add_entity_edge(self, id1, label1, id2, label2, predicate):
     records, summary, keys = self.driver.execute_query('match (a {id: "$id1", label: "$label1"}), (b {id: "$id2", label: "$label2"}) merge (a)-[r:HAS_RELATION]->(b) set r.type = $type;', id1 = id1, label1 = label1, id2 = id2, label2 = label2, type = predicate, database_ = self.db)
   def add_entities_to_graph(self, entities):
-    for entity in entities.entities:
-      ent_name = entity.entity
-      ent_label = entity.category
-      props = entity.properties
-      off_name = entity.official_name
+    for entity in entities:
+      ent_name = entity['entity']
+      ent_label = entity['category']
+      props = entity['properties']
+      off_name = entity['official_name']
       # add entity node
       self.add_entity_node(id = ent_name, name = ent_name, label = ent_label, properties = props)
       self.add_entity_node(id = off_name, name = off_name, label = ent_label, properties = props)
@@ -48,28 +48,42 @@ class KAGExtractor(object):
     hash_hex = has_object.hexdigest()
     records, _, keys = self.driver.execute_query('merge (a: Chunk {id: "$id", summary: "$summary", content: "$content"}) return a;', id = hash_hex, summary = summary, content = text, database_ = self.db)
     with entity in entities:
-      ent_name = entity.entity
-      ent_label = entity.category
-      off_name = entity.official_name
+      ent_name = entity['entity']
+      ent_label = entity['category']
       records, _, keys = self.driver.execute_query('match (a: Entity {id: "$name", label: "$category"}), (b: Chunk {id: "$hex"}) merge (a)-[r:BELONGS_TO]->(b);', name = ent_name, category = ent_label, hex = hash_hex, database_ = self.db)
   def add_edges_to_graph(self, triplets, entities):
     for triplet in triplets:
       ent1_name, predicate, ent2_name = triplet[0], triplet[1], triplet[2]
-      matched1 = list(filter(lambda x: x.entity == ent1_name, entities))
+      matched1 = list(filter(lambda x: x['entity'] == ent1_name, entities))
       if len(matched1) != 1:
         print(f'entity {ent_name1} got multiple matches in entity list! skip triplet {triplet}!')
         continue
-      ent1_label = matched1[0].category
-      matched2 = list(filter(lambda x: x.entity == ent2_name, entities))
+      ent1_label = matched1[0]['category']
+      matched2 = list(filter(lambda x: x['entity'] == ent2_name, entities))
       if len(matched2) != 1:
         print(f'entity {ent_name2} got multiple matches in entity list! skip triplet {triplet}!')
         continue
-      ent2_label = matched2[0].category
+      ent2_label = matched2[0]['category']
       self.add_entity_edge(id1 = ent1_name, label1 = ent1_label, id2 = ent2_name, label2 = ent2_label, predicate = predicate)
   def extract(self, text: str, summary: str):
     entities = self.ner_extract.invoke({'query': text})
-    entities = self.entity_standard.invoke({'query': text, 'entities': str(entities)})
-    self.add_entities_to_graph(entities)
-    triplets = self.triplet_extract.invoke({'query': text, 'entities': str(entities)})
+    import pdb; pdb.set_trace()
+    ents_str = str([{'entity': entity.entity, 'category': entity.category} for entity in entities.entities])
+    ents_with_offname = self.entity_standard.invoke({'query': text, 'entities': ents_str})
+    final_entities = list()
+    for ent in ents_with_offname.entities:
+      matches = list(filter(lambda x: x.entity == ent.entity and x.category == ent.category, entities.entities))
+      if len(matches) != 1:
+        print(f"entity {ent.entity} cannot match single match in entityes! skip this entity!")
+        continue
+      final_entity = {
+        'entity': ent.entity,
+        'category': ent.category,
+        'properties': matches[0].properties,
+        'official_name': ent.official_name
+      }
+      final_entities.append(final_entity)
+    self.add_entities_to_graph(final_entities)
+    triplets = self.triplet_extract.invoke({'query': text, 'entities': ents_str})
     self.add_edges_to_graph(triplets, entities)
     self.add_chunk_to_graph(text, summary, entities)
