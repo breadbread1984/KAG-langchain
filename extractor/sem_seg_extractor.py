2a7cac2e07a4c4e1c4d364265cb565d22116613d
breadbread1984 <breadbread1984@163.com>
2024-11-08 06:43:48 +0000

    update

--- extractor/sem_seg_extractor.py
+++ extractor/sem_seg_extractor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from fuzzywuzzy import process
+import regex as re
 from os.path import join, exists, splitext
 from langchain_community.document_loaders import UnstructuredPDFLoader, TextLoader, UnstructuredMarkdownLoader
 from .tools import load_semantic_seg
@@ -11,6 +11,18 @@ class SemanticSegmentExtractor(object):
     if tokenizer is None or llm is None:
       tokenizer, llm = Qwen2(locally = True)
     self.semantic_seg = load_semantic_seg(tokenizer, llm, 'zh')
+  def fuzzy_find_with_first_char_match(self, text, query, max_l_dist=3):
+    # 确保第一个字符匹配
+    first_char = query[0]
+    pattern = f"(?<={first_char})({query[1:]}){{e<={max_l_dist}}}"
+    matches = list(re.finditer(pattern, text))
+    # 过滤匹配以确保第一个字符相等
+    filtered_matches = [
+        (text[match.start()-1:match.end()], match.start() - 1, match.end())
+        for match in matches
+        if match.start() > 0 and text[match.start() - 1] == first_char
+    ]
+    return filtered_matches
   def extract(self, file_path: str):
     stem, ext = splitext(file_path)
     if ext.lower() == '.md':
@@ -29,8 +41,8 @@ class SemanticSegmentExtractor(object):
     for idx, section in enumerate(sections.sections):
       pos = text.find(section.section_starting_point, beg)
       if pos < 0:
-        matches = process.extract(text, section.section_starting_point, limit = 3)
-        matches = list(filter(lambda x: x[0] == section.section_starting_point[0] and x[1] >= beg, matches))
+        matches = self.fuzzy_find_with_first_char_match(text, section.section_starting_point)
+        matches = list(filter(lambda x: x[1] >= beg, matches))
         if len(matches) == 0:
           raise Exception('unmatched section start string!')
         pos = matches[0][1]
